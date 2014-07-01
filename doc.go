faa59ae2d443b7977b646fe2e48b136890653d94
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-01 10:37:39 -0600

    Add basic structure

---
+++ doc.go
@@ -0,0 +1,7 @@
+// package qp implements the "QP" protocol found at qp.github.io
+//
+// The QP protocol allows for agnostic communication with any underlying
+// message queue system. By using QP, you remove technology-dependent
+// code from your application, while gaining additional functionality,
+// such as QP's pipeline concept.
+package qp
