8cca9c84731c45bc3dbd5fd5c11df3a0fcf33032
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-01 11:11:24 -0600

    Add transport interface

--- transports/transport.go
+++ transports/transport.go
@@ -1,1 +1,28 @@
 package transport
+
+import (
+	"github.com/qp/go/shared"
+)
+
+// BinaryMessage is used to communicate both the
+// topic of the message and the associated data.
+type BinaryMessage struct {
+	topic string
+	data  []byte
+}
+
+// MessageFunc is the signature for a Message Received Callback
+type MessageFunc func(message *BinaryMessage)
+
+// Transport is an interface declaring functions used
+// for interacting with an underlying transport technology
+// such as nsq or rabbitmq.
+type Transport interface {
+	// listen for a message on the given topic
+	// must be called before calling Start
+	ListenFor(topic string)
+	OnMessage(callback MessageFunc)     // set the function to be called when a message is received
+	Send(message *shared.Message) error // send a message to the queue
+	Start()                             // start processing messages
+	Stop()                              // gracefully stop processing messages
+}
