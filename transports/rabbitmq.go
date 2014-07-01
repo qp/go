d3103e7ea77414bbee76556701da4e71c095f16d
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-01 15:59:44 -0600

    Update stub

--- transports/rabbitmq.go
+++ transports/rabbitmq.go
@@ -1,9 +1,6 @@
 package transport
 
-import "github.com/qp/go/shared"
-
-// ensure RabbitMQ conforms to Transport interface
-var conformsRabbitMQ = Transport(&RabbitMQ{})
+import "github.com/qp/go/messages"
 
 // RabbitMQ is the RabbitMQ implementation of the
 // Transport interface. It provides all functionality
@@ -12,28 +9,31 @@ var conformsRabbitMQ = Transport(&RabbitMQ{})
 type RabbitMQ struct {
 }
 
-// ListenFor instructs RabbitMQ to deliver a message for the given topic
-func (r *RabbitMQ) ListenFor(topic string) {
+// NewRabbitMQ creates a RabbitMQ instance and dials the connection
+// to the server.
+func NewRabbitMQ(url string) (*RabbitMQ, error) {
+	return nil, nil
+}
 
+// ListenFor instructs RabbitMQ to deliver a message for the given topic
+func (r *RabbitMQ) ListenFor(topic string) error {
+	return nil
 }
 
 // OnMessage sets the callback function to call when a message is received
 func (r *RabbitMQ) OnMessage(callback MessageFunc) {
-
 }
 
 // Send sends a message out to RabbitMQ
-func (r *RabbitMQ) Send(message *shared.Message) error {
+func (r *RabbitMQ) Send(message *messages.Message) error {
 	return nil
 }
 
 // Start begins processing messages to/from RabbitMQ
 func (r *RabbitMQ) Start() {
-
 }
 
 // Stop gracefully stops processing messages, allowing in-flight
 // requests to finish before stopping entirely
 func (r *RabbitMQ) Stop() {
-
 }
