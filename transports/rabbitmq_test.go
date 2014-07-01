2d880e3efb763ced1598e46d37351759ed78f529
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-01 16:00:04 -0600

    Add rabbitmq_test.go
    Fix #2

---
+++ transports/rabbitmq_test.go
@@ -0,0 +1,30 @@
+package transport
+
+import "os/exec"
+
+// ensure RabbitMQ conforms to Transport interface
+var conformsRabbitMQ = Transport(&RabbitMQ{})
+
+// initRabbitMQ does the following:
+// 1. Checks if rabbitmq is installed
+// 2. Checks if rabbitmq is running
+// 3. Starts it if it is not
+func initRabbitMQ() bool {
+	err := exec.Command("which", "rabbitmqctl").Run()
+	if err != nil {
+		return false
+	}
+	err = exec.Command("rabbitmqctl", "status").Run()
+	if err != nil {
+		// rabbitmq is not running. Run it.
+		err = exec.Command("rabbitmq-server", "-detached").Run()
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+func stopRabbitMQ() {
+	exec.Command("rabbitmqctl", "stop").Run()
+}
