4e93a463277f74cb9fac60898fe2a88a3ed17616
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-02 09:45:23 -0600

    shared->utils

--- messages/message.go
+++ messages/message.go
@@ -2,8 +2,7 @@ package messages
 
 import (
 	"code.google.com/p/go-uuid/uuid"
-
-	"github.com/qp/go/shared"
+	"github.com/qp/go/utils"
 )
 
 // Message is the standard QP messaging object.
@@ -11,11 +10,11 @@ import (
 // QP nodes, as well as containing the metadata
 // necessary to implement the pipeline functionality.
 type Message struct {
-	To   shared.StringDES // array of destination addresses
-	From shared.StringDES // array of addresses encountered thus far
-	ID   string           // a UUID identifying this message
-	Data interface{}      // arbitrary data payload
-	Err  interface{}      // arbitrary error payload. nil if no error
+	To   utils.StringDES // array of destination addresses
+	From utils.StringDES // array of addresses encountered thus far
+	ID   string          // a UUID identifying this message
+	Data interface{}     // arbitrary data payload
+	Err  interface{}     // arbitrary error payload. nil if no error
 }
 
 // NewMessage creates a new Message object with appropriate fields set.
