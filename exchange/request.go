57b6f7c1d37c5c5d0abbd4a4b74f1b3e60f9e640
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-17 10:55:52 -0600

    Put contents of Response directly in Request

--- exchange/request.go
+++ exchange/request.go
@@ -6,11 +6,13 @@ import "github.com/qp/go/utils"
 // in the standard qp request object. It is used
 // as part of the RequestHandler callback.
 type Request struct {
-	*Response
-	To []string `json:"to"` // array of destination addresses
+	To   []string    `json:"to"`   // array of destination addresses
+	From []string    `json:"from"` // array of addresses encountered thus far
+	ID   string      `json:"id"`   // a UUID identifying this message
+	Data interface{} `json:"data"` // arbitrary data payload
 }
 
 // MakeRequest makes a new request object and generates a unique ID in the from array.
 func MakeRequest(endpoint string, object interface{}, pipeline ...string) *Request {
-	return &Request{To: pipeline, Response: MakeResponse(endpoint, object, utils.UniqueStringID())}
+	return &Request{To: pipeline, From: []string{endpoint}, ID: utils.UniqueStringID(), Data: object}
 }
