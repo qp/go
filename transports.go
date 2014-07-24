120b4a46935689fdf1be3eb2d8aa4e57e2b6e3dd
Mat Ryer <mat@stretchr.com>
2014-07-24 16:53:24 -0600

    updated comments

--- transports.go
+++ transports.go
@@ -20,27 +20,44 @@ type Message struct {
 // MessageFunc is the signature for a Message Received Callback.
 type MessageFunc func(bm *Message)
 
-// transport is an interface declaring functions used
-// for interacting with an underlying transport technology
-// such as nsq or rabbitmq.
-type transport interface {
-	Start() error
-	Stop()
-	OnMessage(messageFunc MessageFunc)
-	SetTimeout(timeout time.Duration)
-	ListenFor(channel string)
-	Send(to string, data []byte) error
-}
-
 // RequestTransport describes types capable of making Requests and
 // getting responses.
 type RequestTransport interface {
-	transport
+	// Starts the transport.
+	Start() error
+	// Stops the transport.
+	Stop()
+	// Registers a MessageFunc the will be called when a
+	// message is received.
+	OnMessage(messageFunc MessageFunc)
+	// SetTimeout sets the amount of time in-flight requests have
+	// to complete before being shut down.
+	SetTimeout(timeout time.Duration)
+	// ListenFor listens for messages on the specified channel.
+	ListenFor(channel string)
+	// Send sends a message of data to the specified destination.
+	Send(to string, data []byte) error
 }
 
 // EventTransport describes types capable of firing and listening
 // for events.
 type EventTransport interface {
-	transport
+	// Starts the transport.
+	Start() error
+	// Stops the transport.
+	Stop()
+	// Registers a MessageFunc the will be called when a
+	// message is received.
+	OnMessage(messageFunc MessageFunc)
+	// SetTimeout sets the amount of time in-flight requests have
+	// to complete before being shut down.
+	SetTimeout(timeout time.Duration)
+	// ListenFor listens for messages on the specified channel.
+	ListenFor(channel string)
+	// Send sends a message of data to the specified destination.
+	Send(to string, data []byte) error
+	// ListenForChildren listens for messages on the specified channel
+	// or any of its children. See PubSub.SubscribeChildren for more
+	// information.
 	ListenForChildren(channel string)
 }
