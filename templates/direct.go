b25b471a3864d713105621717f1e06894afd079f
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-29 16:16:16 -0600

    Add Direct template

---
+++ templates/direct.go
@@ -0,0 +1,51 @@
+package templates
+
+import (
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/qp/go"
+	"github.com/stretchr/pat/stop"
+)
+
+// Direct represents a qp.DirectTransport.
+type Direct struct {
+	lock     sync.RWMutex
+	handlers map[string]qp.Handler
+	stopChan chan stop.Signal
+}
+
+// ensure the interface is satisfied
+var _ qp.DirectTransport = (*Direct)(nil)
+
+// Start starts the transport.
+func (d *Direct) Start() error {
+	return errors.New("not implemented")
+}
+
+// Stop instructs the transport to gracefully stop and close the
+// StopChan when stopping has completed.
+//
+// In-flight requests will have "wait" duration to complete
+// before being abandoned.
+func (d *Direct) Stop(wait time.Duration) {
+
+}
+
+// StopChan gets the stop channel which will block until
+// stopping has completed, at which point it is closed.
+// Callers should never close the stop channel.
+func (d *Direct) StopChan() <-chan stop.Signal {
+	return stop.Stopped()
+}
+
+// Send sends data on the channel.
+func (d *Direct) Send(channel string, data []byte) error {
+	return errors.New("not implemented")
+}
+
+// OnMessage binds the handler to the specified channel.
+func (d *Direct) OnMessage(channel string, handler qp.Handler) error {
+	return errors.New("not implemented")
+}
