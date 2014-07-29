c13b2fc5fc68dbee138da56e29bf39bb039ee983
Tyler Bunnell <tylerbunnell@gmail.com>
2014-07-29 16:11:38 -0600

    InProc PubSubTransport complete

--- inproc/pubsub.go
+++ inproc/pubsub.go
@@ -1,7 +1,6 @@
 package inproc
 
 import (
-	"errors"
 	"sync"
 	"time"
 
@@ -9,8 +8,12 @@ import (
 	"github.com/stretchr/pat/stop"
 )
 
+var nothing = struct{}{}
+
 // PubSub represents a qp.PubSubTransport.
 type PubSub struct {
+	lock     sync.RWMutex
+	handlers map[string]qp.Handler
 	stopChan chan stop.Signal
 }
 
@@ -18,25 +21,60 @@ type PubSub struct {
 var _ qp.PubSubTransport = (*PubSub)(nil)
 
 var queue = make(chan *qp.Message)
-var listeners = make(map[string][]*PubSub)
+var instances = make(map[*PubSub]struct{})
 var lock sync.RWMutex
 
 // NewPubSub makes a new PubSub.
 func NewPubSub() *PubSub {
 	p := &PubSub{
+		handlers: make(map[string]qp.Handler),
 		stopChan: stop.Make(),
 	}
+	lock.Lock()
+	instances[p] = nothing
+	lock.Unlock()
 	return p
+}
+
+func processMessages() {
+	go func() {
+		for {
+			select {
+			case m, ok := <-queue:
+				if !ok {
+					return
+				}
+				lock.Lock()
+				for instance := range instances {
+					instance.lock.RLock()
+					if h, ok := instance.handlers[m.Source]; ok {
+						go h.Handle(m)
+					}
+					instance.lock.RUnlock()
+				}
+				lock.Unlock()
+			}
+		}
+	}()
+}
+
+func init() {
+	processMessages()
 }
 
 // Publish publishes data on the specified channel.
 func (p *PubSub) Publish(channel string, data []byte) error {
-	return errors.New("not implemented")
+	m := &qp.Message{Source: channel, Data: data}
+	queue <- m
+	return nil
 }
 
 // Subscribe binds the handler to the specified channel.
 func (p *PubSub) Subscribe(channel string, handler qp.Handler) error {
-	return errors.New("not implemented")
+	p.lock.Lock()
+	p.handlers[channel] = handler
+	p.lock.Unlock()
+	return nil
 }
 
 // Start starts the transport.
@@ -46,7 +84,9 @@ func (p *PubSub) Start() error {
 
 // Stop stops the transport and closes StopChan() when finished.
 func (p *PubSub) Stop(time.Duration) {
-	// do work to stop
+	lock.Lock()
+	delete(instances, p)
+	lock.Unlock()
 	close(p.stopChan)
 }
 
